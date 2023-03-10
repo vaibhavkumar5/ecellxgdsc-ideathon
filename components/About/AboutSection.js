fe18f62b870aaa3a039050a536adb79b356e3168
vaibhav kumar <77304656+vaibhavkumar5@users.noreply.github.com>
2023-03-11 02:59:45 +0530

    rule/timeline update -v

--- components/About/AboutSection.js
+++ components/About/AboutSection.js
@@ -6,25 +6,137 @@ function AboutSection() {
   return (
     <div id="about" className="content-center p-4 mb-14 sm:p-8">
       <div className="hidden md:inline">
-        <SubHeading head="ABOUT IDEATHON 2k23" />
+        <SubHeading head="Rule Book" />
       </div>
       <div className="md:hidden">
-        <SubHeading head="ABOUT " />
-        <SubHeading head="IDEATHON 2k23" />
+        <SubHeading head="Rule Book " />
       </div>
       <div className="w-[100%] md:w-4/5 mx-auto space-y-8 mt-8 text-[#432d1d]">
         <div className="font-consolas text-justify text-xl md:text-2xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Welcome to the Ideathon organized by Ecell and GDSC! We are excited to
+          have you participate in this event where you will have the opportunity
+          to showcase your innovative ideas and turn them into reality. In this
+          document, we will outline the rules and procedures for the Ideathon,
+          as well as provide you with information on how to prepare for the
+          event.
         </div>
+
+        <div className="hidden md:inline">
+          <SubHeading head="Rules" />
+        </div>
+        <div className="md:hidden">
+          <SubHeading head="Rules " />
+        </div>
+
         <div className="font-consolas text-justify text-xl md:text-2xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <ol>
+            <li>
+              {" "}
+              1. The Ideathon will take place offline, and it will be held at
+              Utkarsh, the annual cultural-techno fest of ADGITM (formerly known
+              as Northern India Engineering College).
+            </li>
+
+            <li className="pt-4">
+              {" "}
+              2. Participants can work individually or in teams of up to 4
+              members.
+            </li>
+
+            <li className="pt-4">
+              {" "}
+              3. The theme for the Ideathon is "Innovative Solutions for
+              Sustainability".
+            </li>
+
+            <li className="pt-4">
+              {" "}
+              4. The Ideathon will take place on March 29th, 2023, starting at
+              9:00 AM and ending at 6:00 PM.
+            </li>
+
+            <li className="pt-4">
+              {" "}
+              5. Participants can start working on their idea from March 25th,
+              2023.
+            </li>
+
+            <li className="pt-4">
+              {" "}
+              6. Participants will be given a problem statement related to
+              sustainability, and they will have to come up with an innovative
+              solution using technology.
+            </li>
+
+            <li className="pt-4">
+              {" "}
+              7. The solution should be feasible, scalable, and have a positive
+              impact on the environment and society.
+            </li>
+
+            <li className="pt-4">
+              {" "}
+              8. Participants will have 8 hours to develop their solution, and
+              they will have to submit a video presentation of their idea along
+              with a prototype or demo.
+            </li>
+
+            <li className="pt-4">
+              {" "}
+              9. The judging criteria will be based on the idea's feasibility,
+              impact, scalability, and innovation. Preparing for the Ideathon:
+            </li>
+          </ol>
         </div>
+            
+        <div className="hidden md:inline">
+          <SubHeading head="Preparing" />
+        </div>
+        <div className="md:hidden">
+          <SubHeading head="Preparing" />
+        </div>
+
+        <div className="font-consolas text-justify text-xl md:text-2xl">
+          <ol>
+            <li>
+              {" "}
+              1.  Participants can start preparing for the Ideathon as soon as possible.
+            </li>
+
+            <li className="pt-4">
+              {" "}
+              2. Participants can start researching and brainstorming ideas related to sustainability, and how technology can be used to solve these problems.
+            </li>
+
+            <li className="pt-4">
+              {" "}
+              3.  Participants can start developing their skills in programming, design, and problem-solving.
+            </li>
+
+            <li className="pt-4">
+              {" "}
+              4. Participants can start forming teams or finding partners to work with.
+            </li>
+
+            <li className="pt-4">
+              {" "}
+              5. Participants can attend workshops and webinars organized by Ecell and GDSC to gain knowledge and skills related to sustainability and technology.
+            </li>
+
+          </ol>
+        </div>
+
+        <div className="font-consolas text-justify text-xl md:text-2xl">
+          We encourage all participants to start preparing early and to come up with innovative ideas that can have a positive impact on the environment and society. We wish you all the best of luck and can't wait to see your ideas come to life at the Ideathon!
+
+        </div>
+
       </div>
       <div className="flex mt-6">
         {/* <button className="mx-auto border p-2 px-3 rounded-lg border-[#432D1D] mt-4 text-lg">
           RULEBOOK
         </button> */}
-        <Link href="#">
+        <Link href="https://docs.google.com/document/d/1gSLstdq1Tu9LnQQGkKYG7WtPGyzwo2Q7vQHTwlZifkY/edit?usp=sharing">
           <a
             target="_blank"
             type="submit"
--- components/Time/React_Timeline.js
+++ components/Time/React_Timeline.js
@@ -4,7 +4,7 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import { MdOutlineAppRegistration } from "react-icons/md";
+import { MdOutlineAppRegistration, MdAssignmentTurnedIn, MdDesignServices,MdAssignment} from "react-icons/md";
 
 const React_Timeline = () => {
   return (
@@ -12,90 +12,60 @@ const React_Timeline = () => {
       <VerticalTimeline>
         <VerticalTimelineElement
           className="vertical-timeline-element--content"
-          // contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          // contentStyle={{ width: "30%" }}
-          // contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          date="1st November 2022"
+          
+          date="12 March 2023"
           iconStyle={{ background: "rgba(67, 45, 29, 1)", color: "#fff" }}
           icon={<MdOutlineAppRegistration />}
         >
           <h1 className="font-bold font-gilroy text-xl">Registrations Start</h1>
         </VerticalTimelineElement>
+
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="14th November 2022"
+          date="28 March 2023"
           iconStyle={{ background: "rgba(67, 45, 29, 1)", color: "#fff" }}
-          // icon={<WorkIcon />}
+           icon={<MdAssignmentTurnedIn />}
         >
-          {/* <h3 className="vertical-timeline-element-title">Art Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">
-          San Francisco, CA
-        </h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, SEO, Online
-          Marketing
-        </p> */}
+          
           <h1 className="font-bold font-gilroy text-xl">Registration Ends</h1>
         </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="12-14th November 2022"
-          iconStyle={{ background: "rgba(67, 45, 29, 1)", color: "#fff" }}
-          // icon={<WorkIcon />}
-        >
-          {/* <h3 className="vertical-timeline-element-title">Web Designer</h3>
-        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-        <p>User Experience, Visual Design</p> */}
-          <h1 className="font-bold font-gilroy text-xl">Round 1 Submissions</h1>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="17th November 2022"
-          iconStyle={{ background: "rgba(67, 45, 29, 1)", color: "#fff" }}
-          // icon={<WorkIcon />}
-        >
-          {/* <h3 className="vertical-timeline-element-title">Web Designer</h3>
-        <h4 className="vertical-timeline-element-subtitle">
-          San Francisco, CA
-        </h4>
-        <p>User Experience, Visual Design</p> */}
-          <h1 className="font-bold font-gilroy text-xl">Round 1 Results</h1>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="18-20th November 2022"
-          iconStyle={{ background: "rgba(67, 45, 29, 1)", color: "#fff" }}
-          // icon={<SchoolIcon />}
-        >
-          {/* <h3 className="vertical-timeline-element-title">
 
-          Content Marketing for Web, Mobile and Social Media
-        </h3>
-        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-        <p>Strategy, Social Media</p> */}
-          <h1 className="font-bold font-gilroy text-xl">
-            Breaking Conventions
-          </h1>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="25 March 2023"
+          iconStyle={{ background: "rgba(67, 45, 29, 1)", color: "#fff" }}
+          icon={<MdDesignServices />}
+        >
+          
+          <h1 className="font-bold font-gilroy text-xl">You can start working on your prototype</h1>
+
         </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="1pm  29 March 2022 ADGITM "
+          iconStyle={{ background: "rgba(67, 45, 29, 1)", color: "#fff" }}
+           icon={<MdAssignment />}
+        >
+          
+          <h1 className="font-bold font-gilroy text-xl">Round 1 </h1>
+        </VerticalTimelineElement>
+
+
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="19-20th November 2022"
+          date=" 3:30pm 29 March 2022"
           iconStyle={{ background: "rgba(67, 45, 29, 1)", color: "#fff" }}
-          // icon={<SchoolIcon />}
+          icon={<MdAssignment />}
         >
-          {/* <h3 className="vertical-timeline-element-title">
-          Agile Development Scrum Master
-        </h3>
-        <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-        <p>Creative Direction, User Experience, Visual Design</p> */}
+          
           <h1 className="font-bold font-gilroy text-xl">
-            Finale and Side Events
+            Round 2
           </h1>
         </VerticalTimelineElement>
-        {/* <VerticalTimelineElement
-        iconStyle={{ background: "rgba(67, 45, 29, 1)", color: "#fff" }}
-        // icon={<StarIcon />}
-      /> */}
+
+        
+       
       </VerticalTimeline>
     </div>
   );
